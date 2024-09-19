f8806b2b53013c42b738b14d90fcb3af49448f7f
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-09-19 10:50:26 -0300

    refactor: remove *.c files and update main.c

--- introduction/main.c
+++ introduction/main.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-#include "file1.c"
-#include "file2.c"
-#include "file3.c"
-#include "file4.c"
 
 int main()
 {
-    /*
-    temperature1();
-    temperature2();
-    temperature3();
-    inputAndOutput();
-    */
-    inputAndOutputExercise();
+    printf("hello, world!");
     return 0;
 }
